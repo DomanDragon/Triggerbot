916621006b7d9e7fc1936bf19f4a800677b2a6cc
chrislo27 <kinchrislo@gmail.com>
2016-07-17 20:24:44 -0700

    Added DiscordException message to sendMessage

--- src/chrislo27/bot/bots/baristabot2/BaristaBot2.java
+++ src/chrislo27/bot/bots/baristabot2/BaristaBot2.java
@@ -160,7 +160,8 @@ public class BaristaBot2 extends Bot {
 				if (e.getErrorMessage().contains("502")
 						&& e.getErrorMessage().contains("CloudFlare")) {
 					Main.info(
-							"Exception was a 502, throwing RateLimitException to trick request buffer");
+							"Exception was a 502, throwing RateLimitException to trick request buffer: "
+									+ e.getErrorMessage());
 					throw new RateLimitException("Fake exception thrown because of HTTP 502", 3500,
 							"HTTP 502");
 				}
